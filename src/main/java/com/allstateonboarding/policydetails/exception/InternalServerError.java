501c32146fde25ef4b11a501d1983f788bb12cc5
Lakshmi Prabha P <lakshmi.prabha@thoughtworks.com>
2023-06-05 18:01:50 +0530

    [VLA] Add. Exception for Endpoint

---
+++ src/main/java/com/allstateonboarding/policydetails/exception/InternalServerError.java
@@ -0,0 +1,7 @@
+package com.allstateonboarding.policydetails.exception;
+
+public class InternalServerError extends RuntimeException {
+    public InternalServerError(String message) {
+        super(message);
+    }
+}
