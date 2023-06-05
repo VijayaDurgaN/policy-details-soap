f62051d7ec43fbf7d349dcd75bd305824cdb10f2
VijayaDurgaN <vijaya.durga@thoughtworks.com>
2023-06-05 12:13:14 +0530

    Initial commit

---
+++ src/main/java/com/allstateonboarding/policydetailssoap/PolicyDetailsSoapApplication.java
@@ -0,0 +1,13 @@
+package com.allstateonboarding.policydetailssoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyDetailsSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyDetailsSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstateonboarding/policydetailssoap/PolicyDetailsSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstateonboarding.policydetailssoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyDetailsSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
